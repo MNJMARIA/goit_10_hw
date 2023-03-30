be565053e7062861c581e69a30c1bd9b568fc2fb
Maria <stepanovamasha70@gmail.com>
2023-03-30 19:36:54 +0300

    add task1

--- src/task_1/Work.java
+++ src/task_1/Work.java
@@ -6,7 +6,6 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Work {
-
     public List<String> createList()
     {
         List<String> names= new ArrayList<>();
