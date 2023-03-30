ab9efd824e9b26a782e3fc9875d9384239ff5dac
Maria <stepanovamasha70@gmail.com>
2023-03-30 18:36:59 +0300

    add gitignore

---
+++ src/task_5/Main.java
@@ -0,0 +1,11 @@
+package task_5;
+
+public class Main {
+    /*Завдання 5
+    Напишіть метод public static <T> Stream<T> zip(Stream<T> first, Stream<T> second)
+     який "перемішує" елементи зі стрімів first та second,
+     зупиняючись тоді, коли у одного зі стрімів закінчаться елементи.*/
+    public static void main(String[] args) {
+
+    }
+}
