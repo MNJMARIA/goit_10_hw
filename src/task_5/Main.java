2a0e8cef4d16a0995c3d931bf7ad08aba4d90ff3
Maria <stepanovamasha70@gmail.com>
2023-04-01 17:20:53 +0300

    add task5

--- src/task_5/Main.java
+++ src/task_5/Main.java
@@ -1,11 +1,37 @@
 package task_5;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     /*Завдання 5
     Напишіть метод public static <T> Stream<T> zip(Stream<T> first, Stream<T> second)
      який "перемішує" елементи зі стрімів first та second,
      зупиняючись тоді, коли у одного зі стрімів закінчаться елементи.*/
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Stream<String> first = Stream.of("Bell", "Riuu", "Mariya", "Alize");
+        Stream<String> second = Stream.of("Cranel", "Lion", "Kim");
+
+        Stream<String> result = zip(first, second);
+        //Expect - Bell Cranel Riuu Lion Mariya Kim
+
+        System.out.println("RESULT: ");
+        result.forEach(element -> System.out.print(element + " "));
+    }
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
+        List<T> firstList = first.collect(Collectors.toList());
+        List<T> secondList = second.collect(Collectors.toList());
+
+        int minSize = Math.min(firstList.size(), secondList.size());
+
+        List<T> resultList = new ArrayList<>();
+        for(int i = 0; i < minSize; i++) {
+            resultList.add(firstList.get(i));
+            resultList.add(secondList.get(i));
+        }
+        return resultList.stream();
     }
 }
